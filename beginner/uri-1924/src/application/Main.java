6a0bed76823d4e406d9759edc349ec6f3bde9d1f
Ryann <ryann.arruda_159@hotmail.com>
2021-05-20 23:56:13 -0300

    Problem 1924

---
+++ beginner/uri-1924/src/application/Main.java
@@ -0,0 +1,19 @@
+package application;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i = 0; i < n; i++) {
+			String course = sc.next();
+		}
+		
+		System.out.println("Ciencia da Computacao");
+		
+		sc.close();
+	}
+}
