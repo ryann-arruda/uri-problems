932c58b8d6c288a7bc978a2f3c4191ce96a00bd1
Ryann <ryann.arruda_159@hotmail.com>
2021-06-16 23:18:08 -0300

    Problem 1959

---
+++ beginner/uri-1959/src/application/Main.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long N, L;
+		
+		N = sc.nextLong();
+		L = sc.nextLong();
+		
+		System.out.println(N*L);
+		
+		sc.close();
+	}
+}
